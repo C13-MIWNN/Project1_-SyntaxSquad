78c34eb286c71808597b4c64f9ee8a0603053797
Henk-Jan <aelthir82@gmail.com>
2024-04-12 13:32:36 +0200

    Finished initializer

--- src/main/java/nl/miwnn/cohort13/hashtagsyntaxsquad/recipeproject/controller/InitializeController.java
+++ src/main/java/nl/miwnn/cohort13/hashtagsyntaxsquad/recipeproject/controller/InitializeController.java
@@ -1,90 +1,94 @@
 package nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.controller;
 
+import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.model.AmountOfIngredient;
 import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.model.Recipe;
 import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.model.Ingredient;
 import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.model.Tag;
 
+import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.repositories.AmountOfIngredientRepository;
 import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.repositories.RecipeRepository;
 import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.repositories.IngredientRepository;
 import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.repositories.TagRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.HashSet;
 import java.util.Set;
-/*
- * @author Henk-Jan Veld
+ /** @author Henk-Jan Veld
  * Set some intial data in the database for testing purposes
- **/
+ *
+*/
 @SuppressWarnings("SameReturnValue")
 @Controller
 public class InitializeController {
     private final RecipeRepository recipeRepository;
     private final IngredientRepository ingredientRepository;
     private final TagRepository tagRepository;
+    private final AmountOfIngredientRepository amountOfIngredientRepository;
 
     public InitializeController(RecipeRepository recipeRepository,
                                 IngredientRepository ingredientRepository,
-                                TagRepository tagRepository) {
+                                TagRepository tagRepository, AmountOfIngredientRepository amountOfIngredientRepository) {
         this.recipeRepository = recipeRepository;
         this.ingredientRepository = ingredientRepository;
         this.tagRepository = tagRepository;
+        this.amountOfIngredientRepository = amountOfIngredientRepository;
     }
     @GetMapping("/initialize")
     private String initializeDB() {
 
-        Recipe cheesecake = makeRecipe("Cheesecake");
-        Recipe baklava = makeRecipe("Baklava");
-        Recipe curryPotatoes = makeRecipe("Curry Potatoes");
 
-        Ingredient cheese = makeIngredient("Cheese");
-        Ingredient cake = makeIngredient("Cake");
-        Ingredient phylloDough = makeIngredient("Phyllo Dough");
-        Ingredient sugar = makeIngredient("Sugar");
-        Ingredient potatoes = makeIngredient("Potatoes");
-        Ingredient curry = makeIngredient("Curry");
 
-        Tag soep = makeTag("Soep");
-        Tag taart = makeTag("Taart");
+        Ingredient potatoes = makeIngredient("Potatoes", "piece");
+        Ingredient curry = makeIngredient("Curry", "grams");
+        Ingredient sausages = makeIngredient("Sausages", "grams");
+        Ingredient onions = makeIngredient("Onions", "piece");
+
+        Tag japaneseKitchen = makeTag("Japanese Kitchen");
+
+        AmountOfIngredient gramsSausages = makeAmountOfIngredient(3.00, sausages);
+        AmountOfIngredient gramsCurry = makeAmountOfIngredient(2.00, curry);
+        AmountOfIngredient pieceOfPotato = makeAmountOfIngredient(1.00, potatoes);
+        AmountOfIngredient pieceOfOnion = makeAmountOfIngredient(0.50, onions);
+
+        Recipe curryPotatoes = makeRecipe
+                ("Curry Potatoes", "Combine all ingredients",
+                        Set.of(gramsCurry, gramsSausages, pieceOfPotato, pieceOfOnion), Set.of(japaneseKitchen));
+
+
 
         return "redirect:/";
     }
-    private Ingredient makeIngredient(String name) {
+    private Ingredient makeIngredient(String name, String unitOfMeasurement) {
         Ingredient ingredient = new Ingredient();
         ingredient.setName(name);
+        ingredient.setUnitOfMeasurement(unitOfMeasurement);
         ingredientRepository.save(ingredient);
         return ingredient;
     }
+     private Tag makeTag(String tagName) {
+         Tag tag = new Tag();
+         tag.setTagName(tagName);
+         tagRepository.save(tag);
+         return tag;
+     }
+     private AmountOfIngredient makeAmountOfIngredient(Double amount, Ingredient ingredient) {
+         AmountOfIngredient amountOfIngredient = new AmountOfIngredient();
+         amountOfIngredient.setAmount(amount);
+         amountOfIngredient.setIngredient(ingredient);
+         amountOfIngredientRepository.save(amountOfIngredient);
+         return amountOfIngredient;
+     }
 
-    private Recipe makeRecipe(String recipeName, String instructions, Set<Ingredient> ingredients, Set<Tag> tags) {
+    private Recipe makeRecipe(String recipeName, String instructions,
+                              Set<AmountOfIngredient> amountOfIngredients, Set<Tag> tags) {
         Recipe recipe = new Recipe();
         recipe.setRecipeName(recipeName);
         recipe.setInstructions(instructions);
-        recipe.setIngredients(ingredients);
+        recipe.setAmountOfIngredients(amountOfIngredients);
         recipe.setTags(tags);
-
-        Set<Ingredient> ingredientSet = new HashSet<>();
-        ingredientSet.add(new Ingredient());
-        recipe.setIngredients(ingredientSet);
-
-        Set<Tag> tagSet = new Set<);>
-        tagSet.add(new Tag());
-        recipe.setTags((tagSet));
 
         recipeRepository.save(recipe);
         return recipe;
     }
-//tag needs to be revised, based on the type of set it will become
-    private Tag makeTag(String ) {
-        Tag tag = new Tag();
-        tag.setTag();
-        tagRepository.save(tag);
-        return tag;
-    }
-    /*private Ingredient makeIngredient(String name) {
-        Ingredient ingredient = new Ingredient();
-        ingredient.setName(name);
-        ingredientRepository.save(ingredient);
-        return ingredient;
-}*/
+
 }
