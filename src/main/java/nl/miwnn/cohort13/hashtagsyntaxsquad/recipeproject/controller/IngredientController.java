3d86af15ec1e7a10375a486644be2eb8746d3f45
Henk-Jan <aelthir82@gmail.com>
2024-04-15 15:33:45 +0200

    added ingredient search functionality and service layer

--- src/main/java/nl/miwnn/cohort13/hashtagsyntaxsquad/recipeproject/controller/IngredientController.java
+++ src/main/java/nl/miwnn/cohort13/hashtagsyntaxsquad/recipeproject/controller/IngredientController.java
@@ -1,14 +1,17 @@
 package nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.controller;
 
+import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.Service.IngredientService;
 import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.enums.UnitOfMeasurement;
 import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.model.Ingredient;
 import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.repositories.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,14 +23,16 @@ import java.util.List;
 @Controller
 public class IngredientController {
     private final IngredientRepository ingredientRepository;
+    private final IngredientService ingredientService;
 
-    public IngredientController(IngredientRepository ingredientRepository) {
+    @Autowired
+    public IngredientController(IngredientRepository ingredientRepository, IngredientService ingredientService) {
         this.ingredientRepository = ingredientRepository;
-
+        this.ingredientService = ingredientService;
     }
 
     @GetMapping("/ingredient")
-    private String showAllIngredients(Model model) {
+    public String showAllIngredients(Model model) {
         model.addAttribute("allIngredients", ingredientRepository.findAll());
         model.addAttribute("allUnitsOfMeasurement", Arrays.asList(UnitOfMeasurement.values()));
         model.addAttribute("newIngredient", new Ingredient());
@@ -36,12 +41,21 @@ public class IngredientController {
     }
 
     @PostMapping("/ingredient/new")
-    private String saveOrUpdateIngredient
+    public String saveOrUpdateIngredient
             (@ModelAttribute("newIngredient") Ingredient ingredientToBeSaved, BindingResult result) {
         if (!result.hasErrors()) {
             ingredientRepository.save(ingredientToBeSaved);
         }
 
         return "redirect:/ingredient";
+    }
+
+    @GetMapping("/search")
+    public String searchIngredient(@RequestParam("ingredient") String name, Model model) {
+        List<Ingredient> searchResults = ingredientService.findIngredientByName(name);
+        model.addAttribute("searchResults", searchResults);
+        return "searchResults";
+
+
     }
 }
--- src/main/java/nl/miwnn/cohort13/hashtagsyntaxsquad/recipeproject/repositories/RecipeRepository.java
+++ src/main/java/nl/miwnn/cohort13/hashtagsyntaxsquad/recipeproject/repositories/RecipeRepository.java
@@ -3,5 +3,9 @@ package nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.repositories;
 import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+//    List<Recipe> findByRecipeName(String recipeName);
 }
