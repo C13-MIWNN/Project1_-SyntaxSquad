f2ffe300b32cb516b47584dd9aaa75e5d1df166c
Sami Arslan <s.arslan@st.hanze.nl>
2024-04-24 13:14:37 +0200

    Added test and Changed template

---
+++ src/test/java/nl/miwnn/cohort13/hashtagsyntaxsquad/recipeproject/repositories/FavoriteRecipeRepositoryTest.java
@@ -0,0 +1,36 @@
+package nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.repositories;
+
+import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.model.FavoriteRecipe;
+import nl.miwnn.cohort13.hashtagsyntaxsquad.recipeproject.model.RecipeUser;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class FavoriteRecipeRepositoryTest {
+
+    @Autowired
+    private FavoriteRecipeRepository favoriteRecipeRepository;
+
+    @Autowired
+    private RecipeUserRepository recipeUserRepository;
+
+    @Test
+    public void testCountByRecipeUser() {
+        // Arrange
+        RecipeUser recipeUser = new RecipeUser();
+        recipeUserRepository.save(recipeUser);
+
+        // Act
+        long count = favoriteRecipeRepository.countByRecipeUser(recipeUser);
+        long expectedCount = favoriteRecipeRepository.findByRecipeUser(recipeUser).size();
+        // long expectedCount = 0;
+
+        // Assert
+        assertEquals(expectedCount, count);
+    }
+}
+
+
